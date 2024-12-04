0507b6dc88b16cfaf4e91a4ed346721b287b369a
sysu-JohnLiu <2398549260@qq.com>
2024-12-04 14:58:59 +0800

    Update userinfo.h

--- userinfo.h
+++ userinfo.h
@@ -5,42 +5,36 @@
 #include <QLabel>
 #include <QPushButton>
 
-class UserInfo : public QWidget
-{
+class UserInfo : public QWidget {
     Q_OBJECT
 
 public:
     explicit UserInfo(QWidget *parent = nullptr);
     ~UserInfo();
 
+    void loadUserData(const QString &username); // 加载用户数据
+
 private slots:
-    void onEditPhone();      // 修改手机号
-    void onEditEmail();      // 修改邮箱
-    void onEditNickname();   // 修改昵称
-    void onEditName();       // 修改姓名
-    void onEditGender();     // 修改性别
-    void onEditBirthday();   // 修改生日
-    void onEditAvatar();     // 修改头像
-    void onEditPassword();   // 修改密码（新增）
+    void onEditAvatar();
+    void onEditPhone();
+    void onEditEmail();
+    void onEditName();
+    void onEditGender();
+    void onEditBirthday();
+    void onEditPassword();
 
 private:
-    QLabel *avatarLabel;       // 显示头像
-    QLabel *phoneLabel;        // 显示手机号
-    QLabel *emailLabel;        // 显示邮箱
-    QLabel *nicknameLabel;     // 显示昵称
-    QLabel *nameLabel;         // 显示姓名
-    QLabel *genderLabel;       // 显示性别
-    QLabel *birthdayLabel;     // 显示生日
-    QLabel *passwordLabel;     // 显示密码（新增）
+    void saveUserData(const QString &field, const QString &value); // 保存到数据库
 
-    QPushButton *avatarEditButton;   // 修改头像按钮
-    QPushButton *phoneEditButton;    // 修改手机号按钮
-    QPushButton *emailEditButton;    // 修改邮箱按钮
-    QPushButton *nicknameEditButton; // 修改昵称按钮
-    QPushButton *nameEditButton;     // 修改姓名按钮
-    QPushButton *genderEditButton;   // 修改性别按钮
-    QPushButton *birthdayEditButton; // 修改生日按钮
-    QPushButton *passwordEditButton; // 修改密码按钮（新增）
+    QLabel *avatarLabel;
+    QLabel *phoneLabel;
+    QLabel *emailLabel;
+    QLabel *nicknameLabel;
+    QLabel *nameLabel;
+    QLabel *genderLabel;
+    QLabel *birthdayLabel;
+    QLabel *passwordLabel;
+    QPushButton *avatarEditButton;
 };
 
 #endif // USERINFO_H
